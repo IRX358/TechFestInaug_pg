cfb8629ee75a372b3410981898894890cd511934
Irfan IR <ir.358.786@gmail.com>
2025-10-18 11:10:50 +0530

    logo changes

--- src/components/maineventpage.jsx
+++ src/components/maineventpage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import main from '../assets/imgs/main.jpg';
 import PU from '../assets/imgs/PUlogo.jpg';
+import ieeebg from '../assets/imgs/ieeebang.jpg';
+import ieeelg from '../assets/imgs/ieeelogo.jpg';
 
 const MainEventPage = () => {
   return (
@@ -13,12 +15,21 @@ const MainEventPage = () => {
           />
           <p className="subheading">SOCSE & SOISE presents</p>
           <h1 className="main-heading">IEEE Tech Week 2025</h1>
+          <img 
+            src={ieeelg}
+            alt="IEEE Logo" 
+            className="logo-placeholder2" 
+          />
+           <img 
+            src={ieeebg}
+            alt="IEEE blore Logo" 
+            className="logo-placeholder2 gap" 
+          />
           <p className="quote">
             "Innovation for better tomorrow"
           </p>
         </div>
         <div className="image-column">
-         
             <img 
             src={main}
             alt="IEEE Tech Week Event" 
